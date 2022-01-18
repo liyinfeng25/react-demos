0369f3a90ec36db01be46449359a10a5445ec249
liyinfeng02 <liyinfeng02@meituan.com>
2022-01-18 20:43:29 +0800

    20220118

--- src/pages/fallsFlow/h5.tsx
+++ src/pages/fallsFlow/h5.tsx
@@ -1,12 +1,84 @@
-import React from 'react';
-import './index.less'
+import React from "react";
+import "./h5.less";
+import mockData from "./data.json";
+import Tuijian from './tuijian'
+const COLUMN_COUNT = 2;
 
-function FallsflowH5 () {
-  return (
-    <div className="falls-flow-h5">
-h5
-    </div>
-  )
+
+
+const returnComp = (componentName: string) => {
+  const componentList: any = {
+    Tuijian: Tuijian,
+  };
+
+  const Comp = componentList[componentName]
+  console.log('===>', componentName, Comp);
+  
+  return <Comp />
 }
 
-export default FallsflowH5
+function FallsflowPc() {
+  const [data, setData] = React.useState([[], []]);
+  const [curcolumnIndex, setCurcolumnIndex]: any = React.useState(null);
+  const [curIndex, setCurIndex]: any = React.useState(null);
+
+  React.useEffect(() => {
+    let cur = 0;
+    const data = [[], []];
+    for (let i = 0; i < mockData.body.data.searchVoList.length; i++) {
+      const curArr = [].concat(data[cur], mockData.body.data.searchVoList[i++]);
+      data.splice(cur, 1, curArr);
+      if (cur < COLUMN_COUNT -1) {
+        cur++;
+      } else {
+        cur = 0;
+      }
+    }
+    setData(data);
+  }, []);
+
+  const handleClick = (index: number, imgIndex: number) => {
+    setCurcolumnIndex(index)
+    setCurIndex(imgIndex+1)
+    const newData = [...data]
+    newData[index].splice(imgIndex+1, 0, { isTuijian: true, componentName: 'Tuijian' });
+    console.log('===>', newData);
+    setData(newData);
+
+    setTimeout(() => {
+      setCurIndex(null)
+    }, 400)
+  }
+
+  return (
+    <div className="falls-flow-h5">
+      {data &&
+        data.map((item: any, index: number) => (
+          <div
+            className="column"
+            // style={{ width: `calc((100%)/${COLUMN_COUNT})` }}
+          >
+            {
+              item.map((imgItem: any, imgIndex: number) => (
+                imgItem.isTuijian
+                ? returnComp(imgItem.componentName)
+                : <div className="falls-flow-item" onClick={() => handleClick(index,imgIndex )}
+                  style={ imgIndex === curIndex && curcolumnIndex === index ? {marginTop: '300px'} : {}}
+                >
+                    <div className="img-box">
+                      <img
+                        src={`https://${imgItem.designTemplateImageUrl}?x-oss-process=image/resize,w_600/sharpen,100/format,webp`}
+                        alt=""
+                      />
+                    </div>
+                    <div className="title">{imgItem.templateTitle}</div>
+                  </div>
+                ))
+            }
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default FallsflowPc;
---
+++ src/pages/fallsFlow/tuijian.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './h5.less'
+
+function Tuijian () {
+  return (
+    <div className="tuijian" style={{ }}>
+    推荐模块
+    </div>
+  )
+}
+
+export default Tuijian
